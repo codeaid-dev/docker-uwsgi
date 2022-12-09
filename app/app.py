e9df5a6d6958297f22904210a2005e7513e6c347
codeaid-dev <codeaid.lab@gmail.com>
2022-12-09 16:40:07 +0900

    fix

--- app/app.py
+++ app/app.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     now = datetime.datetime.now().strftime('%Y年%m月%d日%H:%M:%S')
+    return render_template('sample.html', page_name='トップページ！！', time=now)
+
+@app.route('/sample')
+def sample():
+    now = datetime.datetime.now().strftime('%Y年%m月%d日%H:%M:%S')
     return render_template('sample.html', page_name='サンプルページ！！', time=now)
