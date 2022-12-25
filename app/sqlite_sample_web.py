0a48363780545dbb3e29d0a22fcc88145e018f47
codeaid-dev <codeaid.lab@gmail.com>
2022-12-25 22:26:27 +0900

    add/fix

---
+++ app/sqlite_sample_web.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+import datetime
+import sqlite3
+
+dbname = 'sample.db'
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    now = datetime.datetime.now().strftime('%Y年%m月%d日%H:%M:%S.%f')
+    return render_template('sample.html', page_name='トップページ！！', time=now)
+
+@app.route('/sqlitesample')
+def sample():
+    now = datetime.datetime.now().strftime('%Y年%m月%d日%H:%M:%S')
+    res = ''
+    con = sqlite3.connect(dbname)
+    res += '接続成功<br>'
+    cur = con.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        score INTEGER
+    )''')
+    res += 'テーブル作成<br>'
+
+    cur.execute("INSERT INTO users VALUES(1, 'Yamada', 85)")
+    cur.execute("INSERT INTO users VALUES(2, 'Tanaka', 79)")
+    cur.execute("INSERT INTO users VALUES(3, 'Suzuki', 63)")
+    res += 'データ挿入<br>'
+
+    cur.execute("SELECT * FROM users WHERE score >= 70")
+    result = cur.fetchall()
+    res += '70点以上選択<br>'
+    for id,name,score in result:
+        res += f'{id}\t{name}\t{score}<br>'
+
+    cur.execute("DROP TABLE users")
+    res += 'テーブル削除<br>'
+
+    con.commit()
+    con.close()
+    return render_template('sqlitesample.html', page_name='SQLiteサンプルページ！！', time=now, result=res)
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
